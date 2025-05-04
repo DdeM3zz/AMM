0f201d999bba1466ad09c660873e471ab0171374
DdeM3zz <mefodevmixail@gmail.com>
2025-05-04 16:49:45 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [537413528] = "", - BABFT
+  [537413528] = "https://raw.githubusercontent.com/DdeM3zz/AMM/refs/heads/main/BABFT.lua", - BABFT
   [000] = "",
   [000] = "", 
   [000] = "", 
